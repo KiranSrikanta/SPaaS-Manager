591232fa64d8ab2f919d64a28d8bc3dab1e6219a
Jithesh <kuzhij1@INGSKUZHIJ1L2C>
2016-05-26 09:54:47 +0530

    Added UI Project

---
+++ src/EMC.SPaaS.Client/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMC.SPaaS.Client.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
